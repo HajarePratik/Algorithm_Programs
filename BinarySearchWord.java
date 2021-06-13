b61fb17158c2c204d49fb5b6932868e809e710f8
HajarePratik <pratikhajare09@gmail.com>
2021-06-13 19:17:45 +0530

    Finding a String Using Binary Search

---
+++ BinarySearchWord.java
@@ -0,0 +1,46 @@
+package com.datastructure.datastucture_programs;
+
+public class BinarySearchWord 
+{
+
+	public static int binarySearch(String[] arr, String Name) 
+	{
+		int a = 0, b = arr.length - 1;
+		while (a <= b) 
+		{
+			int m = a + (b - a) / 2;
+
+			int res = Name.compareTo(arr[m]);
+
+			// Check if x is present at mid
+			if (res == 0)
+				return m;
+
+			// If x greater, ignore left side
+			if (res > 0)
+				a = m + 1;
+
+			// If x is smaller, ignore right side
+			else
+				b = m - 1;
+		}
+
+		return -1;
+	}
+
+	public static void main(String[] args) 
+	{
+
+		// Passing a value to the array
+		String[] arr = { "Pratik", "Murtaza", "Sandesh", "Parth" };
+		String Name = "Sandesh";
+		int result = binarySearch(arr, Name);
+
+		// Checking the value is present or not
+		if (result == -1)
+			System.out.println("Name is not Present");
+		else
+			System.out.println("Name is Present at " + "Position " + result);
+	}
+
+}
